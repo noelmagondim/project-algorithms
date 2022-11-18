a3f573c99899e3035efddd8ce5cc583853f88a97
noelmagondim <noelmagondim@gmail.com>
2022-11-18 16:22:05 -0300

    Fix lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,4 +5,5 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if low_index >= high_index:
         return True
-    return True and is_palindrome_recursive(word, low_index + 1, high_index -1)
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
