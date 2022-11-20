dbd7cc1b5472d2222acd64d49027388710b324b3
noelmagondim <noelmagondim@gmail.com>
2022-11-20 11:45:27 -0300

    Req 04: in progress

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,18 @@
+def sort_string(any_string):
+    sort = ""
+    for str in any_string:
+        if str < any_string[0]:
+            sort = any_string + sort
+        else:
+            sort = sort + any_string
+    return sort
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    string_1 = sort_string(first_string.lower())
+    string_2 = sort_string(second_string.lower())
+    if string_1 == "" and string_2 == "":
+        return (string_1, string_2, False)
+    if string_1 != string_2:
+        return (string_1, string_2, False)
+    if string_1 == string_2:
+        return (string_1, string_2, True)
