e1540fa88e4212cebbbac5f97e0bc173e7a83cc0
noelmagondim <noelmagondim@gmail.com>
2022-11-20 12:01:34 -0300

    Req 04: in progress

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,16 +2,16 @@ def sort_string(any_string):
     sort = ""
     for str in any_string:
         if str < any_string[0]:
-            sort = any_string + sort
+            sort = str + sort
         else:
-            sort = sort + any_string
+            sort = sort + str
     return sort
 
 
 def is_anagram(first_string, second_string):
     string_1 = sort_string(first_string.lower())
     string_2 = sort_string(second_string.lower())
-    if string_1 == "" and string_2 == "":
+    if string_1 == "" or string_2 == "":
         return (string_1, string_2, False)
     if string_1 != string_2:
         return (string_1, string_2, False)
